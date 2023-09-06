35e3426da364568f8ee679a55377eadfdde9a20b
Carlos Diaz <bycdiaz@icloud.com>
2023-09-06 13:29:47 -0700

    Adds game logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,66 @@
+import { useState } from "react"
+import GameInfo from "./components/GameInfo"
+import Header from "./components/Header"
+import MemoryCards from "./components/MemoryCards";
+import { Pokemon } from "./data";
+import './styles/game-container.css'
+
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [currentRoundSelections, setCurrentRoundSelections] = useState([] as Pokemon[]);
+  const [gameOver, setGameOver] = useState(false);
+
   return (
-    <div>
-      wow
+    <div className="game-container">
+      <Header title="Memory Card Game" />
+      <GameInfo
+        currentScore={currentScore}
+        bestScore={bestScore}
+      />
+      {gameView()}
     </div>
   )
+
+  function gameView() {
+    if (gameOver) {
+      return (
+        <>
+          <div>{'Game over!'}</div>
+          <button onClick={() => setGameOver(false)}>{'Play Again'}</button>
+        </>
+      )
+    } else {
+      return (
+        <MemoryCards
+          handleCardClick={handleCardClick}
+        />
+      )
+    }
+  }
+
+  function handleCardClick(pokemonName: Pokemon) {
+    if (currentRoundSelections.includes(pokemonName)) {
+      endGame();
+    } else {
+      continueGame(pokemonName);
+    }
+  }
+
+  function continueGame(pokemonName: Pokemon) {
+    setCurrentScore(currentScore + 1);
+    setCurrentRoundSelections([...currentRoundSelections, pokemonName]);
+  }
+
+  function endGame() {
+    if (currentScore > bestScore) {
+      setBestScore(currentScore);
+    }
+
+    setGameOver(true);
+    setCurrentScore(0);
+    setCurrentRoundSelections([]);
+  }
 }
 
 export default App
